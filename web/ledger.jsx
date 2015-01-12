1b6411600eb3f73384fa707648b9c1592b41abde
Evan Martin <martine@danga.com>
2015-01-11 22:25:02 -0800

    show some dates

--- web/ledger.jsx
+++ web/ledger.jsx
@@ -3,14 +3,28 @@ var util = require('./util');
 var Ledger2 = React.createClass({
   render: function() {
     var entries = this.props.entries.slice(0, 100);
+    //entries = nest.entries(entries);
+    var last = null;
     var rEntries = entries.map((e) => {
-      var tags = (e.tags || []).map((t) => ' #' + t);
+      var date = e.date.slice(0, 7);
+      var next = date;
+      if (last != null) {
+        if (last == date) {
+          date = '';
+        }
+      }
+      last = next;
+
+      var tags = 'no tags';
+      if (e.tags) {
+        tags = e.tags.map((t) => ' #' + t);
+      }
       return (
         <div className="ledger-entry">
+          <div className="ledger-date">{date}</div>
           <div className="ledger-body">
-            <div className="ledger-text">{e.payee}</div>
+            <div className="ledger-text" title={e.date}>{e.payee}</div>
             <div className="ledger-subtext">
-              {e.addr}
               {tags}
             </div>
           </div>
