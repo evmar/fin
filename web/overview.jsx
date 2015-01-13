dc572299b52cd6e44b0ac4591dc620b007fc67e9
Evan Martin <martine@danga.com>
2015-01-13 10:04:44 -0800

    month chart

--- web/overview.jsx
+++ web/overview.jsx
@@ -1,5 +1,5 @@
 
-var Histo = React.createClass({
+var LineChart = React.createClass({
   componentDidMount() {
     var margin = {top:20, right:20, bottom:30, left:70};
     var width = this.props.width - margin.left - margin.right;
@@ -53,6 +53,81 @@ var Histo = React.createClass({
        .datum(entries)
        .attr('class', 'line')
        .attr('d', line);
+  },
+
+  render() {
+    return <div className="graph" />;
+  }
+});
+
+var Histo = React.createClass({
+  componentDidMount() {
+    var margin = {top:20, right:20, bottom:30, left:70};
+    var width = this.props.width - margin.left - margin.right;
+    var height = this.props.height - margin.top - margin.bottom;
+
+    var el = this.getDOMNode();
+    var svg = d3.select(el).append('svg')
+                .attr('width', this.props.width)
+                .attr('height', this.props.height)
+                .append('g')
+                .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    var entries = this.props.entries;
+    entries.sort((a, b) => cmp(a.date, b.date));
+
+
+    var months = d3.nest()
+                   .key((e) => e.date.substr(0, 7))
+                   .sortKeys(d3.ascending)
+                   .rollup((es) => d3.sum(es, (e) => e.amount))
+                   .entries(entries);
+
+    var format = d3.time.format("%Y/%m");
+    months.forEach((m) => {
+      m.month = format.parse(m.key);
+    });
+
+    var x = d3.time.scale()
+              .domain([d3.time.month.offset(months[0].month, -1),
+                       d3.time.month.offset(months[months.length-1].month, 1)])
+              .range([0, width]);
+    var y = d3.scale.linear()
+              .domain(d3.extent(months, (d) => d.values))
+              .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+                  .scale(x)
+                  .orient('bottom');
+    svg.append('g')
+       .attr('class', 'x axis')
+       .attr('transform', 'translate(0,' + height + ')')
+       .call(xAxis);
+
+    var yAxis = d3.svg.axis()
+                  .scale(y)
+                  .orient('left')
+                  .ticks(5)
+                  .tickFormat((d) => '$' + d3.format(',d')(d/100));
+    svg.append('g')
+       .attr('class', 'y axis')
+       .call(yAxis);
+    
+    months.forEach((m) => {
+      var y1 = y(0);
+      var y2 = y(m.values);
+      m.y = d3.min([y1, y2]);
+      m.height = Math.abs(y1 - y2);
+    });
+    var m = months[0].month;
+    var barwidth = x(d3.time.month.offset(m, 1)) - x(m) - 4;
+    svg.selectAll('rect')
+       .data(months)
+       .enter().append('rect')
+       .attr('x', (m) => x(m.month) - barwidth/2)
+       .attr('y', (m) => m.y)
+       .attr('width', barwidth)
+       .attr('height', (m) => m.height)
   },
 
   render() {
