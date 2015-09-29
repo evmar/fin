aa0ea3ed91c49a19fb43594f31b5f1ac6711e543
Evan Martin <martine@danga.com>
2015-09-29 14:20:53 -0700

    move tsconfig into project

--- web/filter.tsx
+++ web/filter.tsx
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-require('./filter.scss');
 import * as util from './util';
 
 export interface Filters {
