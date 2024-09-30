9a45dfbd2b01202e0ea6e0872160dee5afc95a99
Evan Martin <evan.martin@gmail.com>
2024-09-30 09:05:22 -0700

    simplify csv test

--- bank/csv/csv_test.go
+++ bank/csv/csv_test.go
@@ -13,6 +13,27 @@ func date(year, month, day int) time.Time {
 	return time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
 }
 
+func parseAll(input string) ([]*qif.Entry, error) {
+	r := strings.NewReader(input)
+	cr, err := NewCSVReader(r)
+	if err != nil {
+		return nil, err
+	}
+
+	var entries []*qif.Entry
+	for {
+		e, err := cr.ReadEntry()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		entries = append(entries, e)
+	}
+	return entries, nil
+}
+
 func TestCiti(t *testing.T) {
 	const input = `"Status","Date","Description","Debit","Credit"` + "\r\n" +
 		`"Cleared","08/04/2015","FOREIGN TRANSACTION FEE                 
@@ -20,31 +41,23 @@ func TestCiti(t *testing.T) {
 		`"Cleared","08/03/2015","GOOGLE *Music          GOOGLE.COM/CH CA 
 ","",""` + "\r\n"
 
-	r := strings.NewReader(input)
-	cr, err := NewCSVReader(r)
+	entries, err := parseAll(input)
 	if err != nil {
 		panic(err)
 	}
-
 	expects := []qif.Entry{
 		{Date: date(2015, 8, 4), Amount: 311729, Payee: "FOREIGN TRANSACTION FEE", Cleared: 1},
 		{Date: date(2015, 8, 3), Amount: 0, Payee: "GOOGLE *Music          GOOGLE.COM/CH CA", Cleared: 1},
 	}
+
+	if len(entries) != len(expects) {
+		t.Fatalf("got %d entries, want %d", len(entries), len(expects))
+	}
+
 	for i, expect := range expects {
-		e, err := cr.ReadEntry()
-		if err != nil {
-			panic(err)
-		}
+		e := entries[i]
 		if *e != expect {
 			t.Errorf("%d: got\n%#v\nwant\n%#v", i, *e, expect)
 		}
-	}
-
-	_, err = cr.ReadEntry()
-	if err == nil {
-		panic("expected EOF")
-	}
-	if err != io.EOF {
-		panic(err)
 	}
 }
